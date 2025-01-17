36a526ad21192590aeb82280c5d3f5d50de08636
Ojiner <dbstjdrud115@gmail.com>
2025-01-17 09:12:49 +0900

    basic

---
+++ src/main/java/li/com/backend_apiconnet/BackendApiconnetApplication.java
@@ -0,0 +1,13 @@
+package li.com.backend_apiconnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiconnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiconnetApplication.class, args);
+	}
+
+}
