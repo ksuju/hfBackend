27a8cb60394f47bba86458650fd59bbb68d62c76
Ojiner <dbstjdrud115@gmail.com>
2025-01-21 14:12:14 +0900

    mr테스트

--- src/main/java/li/com/backend_apiconnet/controller/KopisController.java
+++ src/main/java/li/com/backend_apiconnet/controller/KopisController.java
@@ -22,7 +22,7 @@ public class KopisController {
     /*
     * 처음부터 모든 데이터를 뿌려주는건 낭비이며, 화면에 전부 표시할수도 없다.
     * 그렇다면, 일부데이터만 화면에 표시후, 더보기 같은 버튼을 사용해야 하나?
-    * 아니면 카카오 지도에 검색하지 않으면 어떤 결과도 표시되지 않게 해야하나.
+    * 아니면 카카오 지도에 검색하지 않으면 어떤 결과도 표시되지 않게 해야하나
     * */
     @GetMapping("")
     public List<KopisFesEntity> getSearchKeyWordList(@RequestParam(value = "keyword" ,required = false)String keyword){
