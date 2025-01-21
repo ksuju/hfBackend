bd5b68d3427a28b92a7a29812bc5f7a981f4dcd4
황민지 <hminji4565@gmail.com>
2025-01-21 14:29:45 +0900

    setting: git commit 테스트

--- src/main/java/com/ll/hfback/domain/home/controller/HomeController.java
+++ src/main/java/com/ll/hfback/domain/home/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping({"/", ""})
     public String mainPage() {
+
         return "Welcome to Main Page";
     }
 }
