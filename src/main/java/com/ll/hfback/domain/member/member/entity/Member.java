c1b7067153709fde5b3529011b50c30fd8dd9711
backnback <garam8796@naver.com>
2025-01-21 16:12:41 +0900

    feat: íêê¸ °본 global 설정 및 member 모듈 기본 ì설정

---
+++ src/main/java/com/ll/hfback/domain/member/member/entity/Member.java
@@ -0,0 +1,26 @@
+package com.ll.hfback.domain.member.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ll.hfback.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class Member extends BaseEntity {
+  @Column(unique = true)
+  private String username;
+  @JsonIgnore
+  private String password;
+  @JsonIgnore
+  private String refreshToken;
+}
