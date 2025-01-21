2b13f03d29a7c3a850b8ec708248865345f2924d
황민지 <hminji4565@gmail.com>
2025-01-21 12:27:56 +0900

    setting: project setting

---
+++ hfback/src/main/java/com/ll/hfback/HfbackApplication.java
@@ -0,0 +1,13 @@
+package com.ll.hfback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HfbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HfbackApplication.class, args);
+	}
+
+}
