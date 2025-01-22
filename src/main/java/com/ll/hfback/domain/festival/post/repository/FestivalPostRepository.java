8a35b47d168266734b1dd735a0f5ba07f5a097e4
KOOTHEMAVERICK <121041290+KOOTHEMAVERICK@users.noreply.github.com>
2025-01-22 20:01:53 +0900

    게시글 조회 기능 구현(ID로 상세 조회, 공연/축제(KOPIS/APIS) 타입으로 조회)

--- src/main/java/com/ll/hfback/domain/festival/post/repository/FestivalPostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ll.hfback.domain.festival.post.repository;
-
-import com.ll.hfback.domain.festival.api.entity.KopisFesEntity;
-import com.ll.hfback.domain.festival.post.entity.FestivalPost;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FestivalPostRepository extends JpaRepository<KopisFesEntity, Long> {
-}
