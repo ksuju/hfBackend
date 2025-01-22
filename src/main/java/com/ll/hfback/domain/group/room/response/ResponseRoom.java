02c1b7c875d7b2ea87eb700f61a2a28e259a669c
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-22 20:33:25 +0900

    feat: 게시글 생성 작업의 응답 처리를 위한 Response 클래스 작성

---
+++ src/main/java/com/ll/hfback/domain/group/room/response/ResponseRoom.java
@@ -0,0 +1,24 @@
+package com.ll.hfback.domain.group.room.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * packageName    : com.ll.hfback.domain.group.room.response
+ * fileName       : ResponseRoom
+ * author         : sungjun
+ * date           : 2025-01-22
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-01-22        kyd54       최초 생성
+ */
+@Getter
+@Setter
+public class ResponseRoom {
+    private Long memberId;
+    private String title;
+    private String content;
+    private int memberLimit;
+}
