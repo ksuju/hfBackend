9d9c3f3eb4890d60e8d197a63e30ed08f9054430
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-23 11:05:50 +0900

    fix: 엔티티 수정에 따른 채팅 메시지 작성 로직 변경 (연관 관계 수정 및 테이블 구조 변경 반영)

--- src/main/java/com/ll/hfback/domain/group/chat/serviceImpl/ChatMessageServiceImpl.java
+++ src/main/java/com/ll/hfback/domain/group/chat/serviceImpl/ChatMessageServiceImpl.java
@@ -1,8 +1,10 @@
 package com.ll.hfback.domain.group.chat.serviceImpl;
 
 import com.ll.hfback.domain.group.chat.dto.response.ResponseMessage;
+import com.ll.hfback.domain.group.chat.entity.Chat;
 import com.ll.hfback.domain.group.chat.entity.ChatMessage;
 import com.ll.hfback.domain.group.chat.repository.ChatMessageRepository;
+import com.ll.hfback.domain.group.chat.repository.ChatRepository;
 import com.ll.hfback.domain.group.chat.service.ChatMessageService;
 import com.ll.hfback.domain.group.room.entity.Room;
 import com.ll.hfback.domain.group.room.repository.RoomRepository;
@@ -31,6 +33,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatMessageServiceImpl implements ChatMessageService {
     private final ChatMessageRepository chatMessageRepository;
     private final RoomRepository roomRepository;
+    private final ChatRepository chatRepository;
     private final MemberRepository memberRepository;
     private final Logger logger = LoggerFactory.getLogger(ChatMessageServiceImpl.class.getName());
 
@@ -38,27 +41,33 @@ public class ChatMessageServiceImpl implements ChatMessageService {
     public void writeMessage(Long roomId, ResponseMessage responseMessage) {
         logger.info("채팅 메시지 작성");
         try {
-            // roomId로 채팅방 정보 가져오기
-            Room room = roomRepository.findById(roomId).get();
+            // roomId로 모임방 정보 가져오기
+            Room room = roomRepository.findById(roomId).orElse(null);
 
-            // memberId로 멤버 정보 가져오기
-            Member member = memberRepository.findById(responseMessage.getMemberId()).get();
+            if(room != null) {
+                // roomId로 채팅방 정보 가져와야함
+                Chat chat = chatRepository.findById(room.getChat().getId()).orElse(null);
 
-            // 채팅 메시지 저장
-            ChatMessage chatMessage = ChatMessage.builder()
-                    .room(room)
-                    .member(member)
-                    .chatMessageContent(responseMessage.getContent())
-                    .build();
+                // memberId로 멤버 정보 가져오기
+                Member member = memberRepository.findById(responseMessage.getMemberId()).orElse(null);
 
-            // Room 엔티티의 chatMessages 리스트에 추가
-            room.getChatMessages().add(chatMessage);
+                // 채팅 메시지 저장
+                ChatMessage chatMessage = ChatMessage.builder()
+                        .chat(chat)
+                        .chatMessageContent(responseMessage.getContent())
+                        .build();
 
-            // Member 엔티티의 chatMessages 리스트에 추가
-            member.getChatMessages().add(chatMessage);
+                // Room 엔티티의 chatMessages 리스트에 추가
+                if(chat != null) {
+                    chat.getChatMessages().add(chatMessage);
+                }
+                
+                chatMessageRepository.save(chatMessage);
+                logger.info("채팅 메시지 작성 완료");
+            } else {
+                logger.error("채팅 메시지 작성 실패, Room or Chat is null");
+            }
 
-            chatMessageRepository.save(chatMessage);
-            logger.info("채팅 메시지 작성 완료");
         } catch (Exception E) { // fix: 어떤 에러가 발생 할 수 있는지, 에러 유형별 처리 방법 생각 (던지기 x)
             logger.error("채팅 메시지 작성 실패 : " + E);
         }
