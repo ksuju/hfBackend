e863c70fb8e0df2533efdd9e78cf2a033761ce74
황민지 <hminji4565@gmail.com>
2025-01-24 17:49:10 +0900

    fix: 테스트 중 하드 url 삭제

--- src/main/java/com/ll/hfback/global/security/ApiSecurityConfig.java
+++ src/main/java/com/ll/hfback/global/security/ApiSecurityConfig.java
@@ -27,7 +27,6 @@ public class ApiSecurityConfig {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.addAllowedOrigin(AppConfig.getSiteFrontUrl());
         corsConfig.addAllowedOrigin(AppConfig.getDevFrontUrl());
-        corsConfig.addAllowedOrigin("https://www.hf.codingtori.site/");
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
 
