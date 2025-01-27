64ddafbfc45288f197982fff0f3f5058b9b608b4
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-27 23:57:37 +0900

    feat: 메시지 읽음 상태 확인을 위한 MessageReadStatusRepository 추가

---
+++ src/main/java/com/ll/hfback/domain/group/chat/repository/MessageReadStatusRepository.java
@@ -0,0 +1,19 @@
+package com.ll.hfback.domain.group.chat.repository;
+
+import com.ll.hfback.domain.group.chat.entity.MessageReadStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * packageName    : com.ll.hfback.domain.group.chat.repository
+ * fileName       : MessageReadStatusRepository
+ * author         : sungjun
+ * date           : 2025-01-27
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-01-27        kyd54       최초 생성
+ */
+public interface MessageReadStatusRepository extends JpaRepository<MessageReadStatus, Long> {
+
+}
