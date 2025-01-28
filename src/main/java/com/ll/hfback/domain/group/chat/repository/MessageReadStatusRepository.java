ad951751a87a176ce50b4eab3fd1f376682ee2f2
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-28 21:07:44 +0900

    feat: 메시지 읽음 상태 확인 기능을 위해 MessageReadStatusRepository에 findByChatRoomIdAndMemberId 메서드 추가

--- src/main/java/com/ll/hfback/domain/group/chat/repository/MessageReadStatusRepository.java
+++ src/main/java/com/ll/hfback/domain/group/chat/repository/MessageReadStatusRepository.java
@@ -3,6 +3,8 @@ package com.ll.hfback.domain.group.chat.repository;
 import com.ll.hfback.domain.group.chat.entity.MessageReadStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 /**
  * packageName    : com.ll.hfback.domain.group.chat.repository
  * fileName       : MessageReadStatusRepository
@@ -15,5 +17,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * 2025-01-27        kyd54       최초 생성
  */
 public interface MessageReadStatusRepository extends JpaRepository<MessageReadStatus, Long> {
-
+    Optional<MessageReadStatus> findByChatRoomIdAndMemberId(Long chatRoomId, Long memberId);
 }
