b085af355895fc16c8ffdc63e82bc079d396e895
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-28 21:05:57 +0900

    feat: 메시지 읽음 상태 확인을 위해 MessageReadStatusResponse 객체 추가

---
+++ src/main/java/com/ll/hfback/domain/group/chat/response/MessageReadStatusResponse.java
@@ -0,0 +1,22 @@
+package com.ll.hfback.domain.group.chat.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * packageName    : com.ll.hfback.domain.group.chat.response
+ * fileName       : MessageReadStatusResponse
+ * author         : sungjun
+ * date           : 2025-01-28
+ * description    : 자동 주석 생성
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025-01-28        kyd54       최초 생성
+ */
+@Getter
+@Setter
+public class MessageReadStatusResponse {
+    private Long memberId;
+    private Long messageId;
+}
