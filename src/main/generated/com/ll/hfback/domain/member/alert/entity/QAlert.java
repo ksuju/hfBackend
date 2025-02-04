784834f9b05a67fa3d4df44372585bfcc89adc40
ksuju <139477852+ksuju@users.noreply.github.com>
2025-02-04 11:56:19 +0900

    chore: q엔티티 업데이트

---
+++ src/main/generated/com/ll/hfback/domain/member/alert/entity/QAlert.java
@@ -0,0 +1,65 @@
+package com.ll.hfback.domain.member.alert.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QAlert is a Querydsl query type for Alert
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAlert extends EntityPathBase<Alert> {
+
+    private static final long serialVersionUID = 1898350581L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QAlert alert = new QAlert("alert");
+
+    public final com.ll.hfback.global.jpa.QBaseEntity _super = new com.ll.hfback.global.jpa.QBaseEntity(this);
+
+    public final StringPath category = createString("category");
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    public final BooleanPath isRead = createBoolean("isRead");
+
+    public final com.ll.hfback.domain.member.member.entity.QMember member;
+
+    public final StringPath url = createString("url");
+
+    public QAlert(String variable) {
+        this(Alert.class, forVariable(variable), INITS);
+    }
+
+    public QAlert(Path<? extends Alert> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QAlert(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QAlert(PathMetadata metadata, PathInits inits) {
+        this(Alert.class, metadata, inits);
+    }
+
+    public QAlert(Class<? extends Alert> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.member = inits.isInitialized("member") ? new com.ll.hfback.domain.member.member.entity.QMember(forProperty("member"), inits.get("member")) : null;
+    }
+
+}
+
