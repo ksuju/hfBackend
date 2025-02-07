3bca61180503e2d366520cfe7e682af36f6b19af
backnback <garam8796@naver.com>
2025-02-07 11:44:20 +0900

    feat: 알림 기능 구현 ì기촄ê¸초기 완료 세부 구현 필요

--- src/main/java/com/ll/hfback/global/websocket/WebSocketConfig.java
+++ src/main/java/com/ll/hfback/global/websocket/WebSocketConfig.java
@@ -34,8 +34,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");  // 메시지 브로커 활성화, 클라이언트가 설정한 경로를 구독하면 서버가 발행하는 메시지를 받을 수 있음
+        registry.enableSimpleBroker("/topic", "/queue", "/user");  // 메시지 브로커 활성화, 클라이언트가 설정한 경로를 구독하면 서버가 발행하는 메시지를 받을 수 있음
         registry.setApplicationDestinationPrefixes("/app"); // 클라이언트가 서버로 메시지를 보낼 때 사용할 경로의 prefix를 지정, ex) /app/message로 메시지를 보내면, 서버에서 해당 경로를 처리함
+        registry.setUserDestinationPrefix("/user");
     }
 
 
